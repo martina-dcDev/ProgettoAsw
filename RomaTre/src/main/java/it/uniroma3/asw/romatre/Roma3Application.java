aa503554c9a8c4cf2b8131593019d223617fcc2a
NaruAna <vivianapilosio17@gmail.com>
2017-05-04 22:01:54 +0200

    Update Roma3Application.java
    Visto che il controller si chiama RomaTreController penso che abbia senso chiamare questa applicazione RomaTreApplication anziché Roma3Application

--- RomaTre/src/main/java/it/uniroma3/asw/romatre/Roma3Application.java
+++ RomaTre/src/main/java/it/uniroma3/asw/romatre/Roma3Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Roma3Application {
+public class RomaTreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Roma3Application.class, args);
+		SpringApplication.run(RomaTreApplication.class, args);
 	}
 }
