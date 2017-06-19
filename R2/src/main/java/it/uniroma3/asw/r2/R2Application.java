e4972cb148c8d4723d8fa2162510d5bdf3b0879c
martina <martinadcor@gmail.com>
2017-06-19 08:26:16 +0200

    modifica R2 per renderlo client Eureka

--- R2/src/main/java/it/uniroma3/asw/r2/R2Application.java
+++ R2/src/main/java/it/uniroma3/asw/r2/R2Application.java
@@ -2,8 +2,10 @@ package it.uniroma3.asw.r2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class R2Application {
 
 	public static void main(String[] args) {
