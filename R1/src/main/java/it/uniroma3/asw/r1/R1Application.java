aafb05e3ed0dd9dbee012c7f938347a94a5aa938
NaruAna <vivianapilosio17@gmail.com>
2017-06-25 22:31:47 +0200

    Modifiche per Eureka all'applicazione principale

--- R1/src/main/java/it/uniroma3/asw/r1/R1Application.java
+++ R1/src/main/java/it/uniroma3/asw/r1/R1Application.java
@@ -2,8 +2,10 @@ package it.uniroma3.asw.r1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class R1Application {
 
 	public static void main(String[] args) {
