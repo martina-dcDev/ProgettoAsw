3e574258db508de4bb2f4c5035d3ce0543003344
NaruAna <vivianapilosio17@gmail.com>
2017-06-26 17:57:26 +0200

    Add files via upload

---
+++ clean-all-projects.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+gradle clean -b Eureka-server/build.gradle 
+gradle clean -b Zuul-server/build.gradle
+
+
+gradle clean -b R1/build.gradle
+gradle clean -b R2/build.gradle
+gradle clean -b RomaTre/build.gradle 
+
+
+
+
